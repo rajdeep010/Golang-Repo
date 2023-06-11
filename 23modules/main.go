58c25b0c8bc24326e65d87ab4333a4c1b2b573cb
Rajdeep <rajdeepmallick999@gmail.com>
2023-06-11 12:42:15 +0530

    crated a router using gorilla mux and handled request to a specific url on localhost

---
+++ 23modules/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Hello mod in golang")
+	greeter()
+
+	// router making
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey Mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to Golang</h1>"))
+}
